cae15221a89126b64199314ecdaa6e2bca149ca8
Igor <igor.kalinowski@infotech.edu.pl>
2025-06-25 23:53:38 +0200

    .

--- MusicApp/MainWindow.xaml.cs
+++ MusicApp/MainWindow.xaml.cs
@@ -1,13 +1,6 @@
-﻿using System.Text;
+﻿using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MusicApp;
 
@@ -19,5 +12,95 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        player.Volume = 0.05;
+
+        if (!Directory.Exists(musicFilesPath))
+        {
+            Directory.CreateDirectory(musicFilesPath);
+            // music download from links.json (maybe txt)
+        }
+        
+    }
+
+    private string musicFilesPath = "C:\\music";    // deafult
+    private int howManySongs;
+    Random random = new Random();
+    MediaPlayer player = new MediaPlayer();
+    private int currentIndex;
+    private int randomIndex = 100;
+    private TimeSpan stopPosition;
+    private bool stopped = false;
+    
+
+
+    private void PlayRandom(object sender, RoutedEventArgs e)
+    {
+        Player();
+    }
+    private void Stop(object sender, RoutedEventArgs e)
+    {
+        stopped = true;
+        player.Pause(); // this exists fuckface
+    }
+    
+    private void changeVolume(object sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+        player.Volume = volumeSlider.Value;
+    }
+
+    private void Player()
+    {
+        howManySongs = Directory.GetFiles(musicFilesPath).Length;
+        if (howManySongs == 0) { MessageBox.Show("check C:/music"); }
+        else
+        {
+            var files = new DirectoryInfo(musicFilesPath).GetFiles();
+
+            if (stopped)
+            {
+                stopped = false;
+                player.Play();
+            }
+            else
+            {
+                randomIndex = random.Next(0, howManySongs);
+                if (currentIndex == randomIndex)
+                {
+                    try
+                    {
+                        currentIndex += 1;
+                    }
+                    catch (Exception e)
+                    {
+                        currentIndex -= 1;
+                    }
+                }
+                else currentIndex = randomIndex;
+
+                var path = files[randomIndex].ToString();
+                player.Open(new Uri(path));
+                player.Play();
+
+                string title = Path.GetFileNameWithoutExtension(path);
+                titleBlock.Text = title;
+            }
+
+            player.MediaEnded += Loop;
+        }
+    }
+
+    private void Loop(object sender, EventArgs e)
+    {
+        if (loopBox.IsChecked == true)
+        {
+            Player();
+        }
+    }
+
+    private void Test(object sender, RoutedEventArgs e)
+    {
+        TimeSpan duration = player.NaturalDuration.TimeSpan;
+        MessageBox.Show("Audio length: " + duration.ToString(@"ss"));
+        
     }
 }
